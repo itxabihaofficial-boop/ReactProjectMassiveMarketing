4dc6e62ed4b258b18713e265bb9b5af77546625f
Abiha <itx.abiha.official@gmail.com>
2025-11-30 02:05:02 +0500

    Text Left Moved

--- src/components/TextRevealSection.tsx
+++ src/components/TextRevealSection.tsx
@@ -13,9 +13,9 @@ const TextRevealSection = () => {
   const words = text.split(" ");
 
   return (
-    <section ref={containerRef} className="min-h-[40vh] flex items-center justify-center align-items-center bg-white pt-[70px] px-8 py-14">
+    <section ref={containerRef} className="min-h-[40vh] flex justify-center bg-white pt-[70px] px-8 py-14">
       <div className="max-w-1xl mx-auto">
-        <p className="text-3xl md:text-7xl sm:text-2xl font-bold justify-center leading-tight  flex flex-wrap gap-x-2 gap-y-1">
+        <p className="text-3xl md:text-7xl sm:text-2xl font-bold leading-tight  flex flex-wrap gap-x-2 gap-y-1">
           {words.map((word, i) => {
             // Calculate range for each word based on total words
             const start = i / words.length;
